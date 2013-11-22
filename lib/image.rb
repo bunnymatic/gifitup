0906ce2a10cbeb825b0e5bb4d2632fab214929f5
Mr Rogers <jon@bunnymatic.com>
2013-11-22 00:32:47 -0800

    Generate an Animated GIF
    * move generate page to index
    * build generate_anim method

--- lib/image.rb
+++ lib/image.rb
@@ -1,7 +1,22 @@
 class Image
 
-  def self.generate_frame(word,opts = {})
+  def self.generate_anim(words, opts = {})
     dest_dir = opts.delete(:dest_dir) || 'public/generated/'
+    frames = words.map{|w| generate_frame(w, dest_dir)}
+    fname = temp_gif(words.join('').gsub(/[[:punct:]]/,''), dest_dir)
+    MojoMagick::convert(nil,fname) do |c|
+      c.delay 40
+      c.loop 0
+      frames.each do |f|
+        c.file f
+      end
+    end
+
+    fname
+  end
+
+
+  def self.generate_frame(word, dest_dir)
     FileUtils.mkdir_p(dest_dir)
     opts = {
       :background => 'black',
@@ -10,15 +25,11 @@ class Image
       :font => 'Helvetica-Bold',
       :pointsize => 72,
       :size => '500x500'
-    }.merge(opts)
+    }
 
     opts[:label] = word
     k = word_as_key(word)
-    fname = 'unk'
-    Dir::Tmpname.create([k,'.gif']) do |path| 
-      fname = File.join(dest_dir, File.basename(path))
-    end
-
+    fname = temp_gif(k, dest_dir) || 'unk'
     #convert -background lightblue -fill blue -font Candice -pointsize 72 label:Anthony label.gif
     MojoMagick::convert(nil, fname) do |c|
       opts.each do |opt,val|
@@ -30,6 +41,14 @@ class Image
 
   def self.tempdir
     Dir::tempdir
+  end
+
+  def self.temp_gif(pfx, dest_dir)
+    fname = nil
+    Dir::Tmpname.create([pfx,'.gif']) do |path| 
+      fname = File.join(dest_dir, File.basename(path))
+    end
+    fname
   end
 
   def self.word_as_key(word)
