9485bbe825df2282f60fb41db5925dd874885713
Mr Rogers <jon@bunnymatic.com>
2013-11-26 02:06:34 -0800

    Start looking into storing images in memcache
    * managing the 'index' will be tough

--- models/image.rb
+++ models/image.rb
@@ -1,6 +1,28 @@
 class Image
   attr_accessor :filename
+  attr_reader :store
+
   def initialize(filename = nil)
     @filename = filename
   end
+
+  def save
+  end
+
+  private
+  def store
+    if ['MEMCACHIER_SERVERS', 'MEMCACHIER_USERNAME', 'MEMCACHIER_PASSWORD'].all?{|k| ENV.has_key? k}
+      memcache_opts = {
+        :username => ENV["MEMCACHIER_USERNAME"],
+        :password => ENV["MEMCACHIER_PASSWORD"],
+        :failover => true,
+        :socket_timeout => 1.5,
+        :socket_failure_delay => 0.2
+      }
+      @store ||= Dalli::Client.new(ENV["MEMCACHIER_SERVERS"].split(","), cache_opts)
+    else
+      @store ||= Dalli::Client.new('localhost:11211')
+    end
+  end
+
 end
