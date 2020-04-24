7730a3bf8bc32d9e2a6b03ded728edd9ef9df807
Jon Rogers <jon@carbonfive.com>
2020-04-24 08:05:52 -0700

    Fix the spinner
    Problem
    --------
    
    We were turning a spinner on but the element was 0 x 0 so the spinner
    wasn't showing.
    
    Solution
    --------
    
    * Fix the spinner size
    * run prettier
    * clean up some console logging

--- public/javascripts/gifitup.js
+++ public/javascripts/gifitup.js
@@ -1,95 +1,93 @@
 $(function() {
-  if ($('form.animator').length) {
+  if ($("form.animator").length) {
     // we're on the index page
-    spinner = new LTSpinner()
+    spinner = new GIUSpinner();
 
-    $('input[type=submit]').on('click', function() {
+    $("input[type=submit]").on("click", function() {
       spinner.spin();
     });
 
     var setTextareaFont = function(font) {
-      $('form textarea[name=words]').css('font-family',font);
+      $("form textarea[name=words]").css("font-family", font);
     };
 
-    $('select.select2').select2();
-    var $fontSelect = $('select[name=font]');
-    $fontSelect.bind('change', function() {
+    $("select.select2").select2();
+    var $fontSelect = $("select[name=font]");
+    $fontSelect.bind("change", function() {
       setTextareaFont($(this).val());
     });
 
     setTextareaFont($fontSelect.val());
 
-    $('#delay-slider').slider({
-      from:0,
+    $("#delay-slider").slider({
+      from: 0,
       to: 3,
       step: 0.1,
       round: 1,
-      dimension: ' s',
-      skin: 'c5',
+      dimension: " s",
+      skin: "c5",
       limits: false
     });
 
-    $('#font-size-slider').slider({
-      from:6,
+    $("#font-size-slider").slider({
+      from: 6,
       to: 200,
       step: 1,
       round: 0,
-      dimension: ' px',
-      skin: 'c5',
+      dimension: " px",
+      skin: "c5",
       limits: false
     });
 
-    var setColor = function(elId,c) {
+    var setColor = function(elId, c) {
       if (c) {
-        $('#'+elId).val(c);
+        $("#" + elId).val(c);
       }
     };
 
     var setBackground = function(bg) {
-      setColor('background', bg);
+      setColor("background", bg);
     };
 
     var setFill = function(fill) {
-      setColor('fill', fill);
+      setColor("fill", fill);
     };
-
 
     var syncColorBoxes = function() {
-      var bg = $('#background').val();
+      var bg = $("#background").val();
       if (bg) {
-        $('.background .color-box').css('background-color', bg);
+        $(".background .color-box").css("background-color", bg);
       }
-      var fg = $('#fill').val();
-      console.log(fg);
+      var fg = $("#fill").val();
       if (fg) {
-        $('.fill .color-box').css('background-color', fg);
+        $(".fill .color-box").css("background-color", fg);
       }
     };
 
-    $('.background .color-box').colpick({
-	    colorScheme:'dark',
-      submit:0,
-	    layout:'hex',
-	    color:'ff00ff',
-	    onChange:function(hsb,hex,rgb, fromColorSet) {
-        var c = '#' + hex;
-        var el = $('.background .color-box');
-		    $(el).css('backgroundColor', c);
+    $(".background .color-box").colpick({
+      colorScheme: "dark",
+      submit: 0,
+      layout: "hex",
+      color: "ff00ff",
+      onChange: function(hsb, hex, rgb, fromColorSet) {
+        var c = "#" + hex;
+        var el = $(".background .color-box");
+        $(el).css("backgroundColor", c);
         setBackground(c);
-	    }
+      }
     });
 
-    $('.fill .color-box').colpick({
-	    colorScheme:'dark',
-      submit:0,
-	    layout:'hex',
-	    color:'cccccc',
-	    onChange:function(hsb,hex,rgb, fromColorSet) {
-        var c = '#' + hex;
-        var el = $('.fill .color-box');
-		    $(el).css('backgroundColor', c);
+    $(".fill .color-box").colpick({
+      colorScheme: "dark",
+      submit: 0,
+      layout: "hex",
+      color: "cccccc",
+      onChange: function(hsb, hex, rgb, fromColorSet) {
+        var c = "#" + hex;
+        var el = $(".fill .color-box");
+        $(el).css("backgroundColor", c);
         setFill(c);
-	    }
+      }
     });
 
     syncColorBoxes();
@@ -102,44 +100,50 @@ $(function() {
      }
     */
     var flash = function(msg) {
-      var $f = $('.flash');
+      var $f = $(".flash");
       if (msg) {
         if (!$f.length) {
-          $f = $('<div>', {'class':'flash'});
-          $('.container').prepend($f);
+          $f = $("<div>", { class: "flash" });
+          $(".container").prepend($f);
         }
         $f.html(msg);
       } else {
         $f.remove();
       }
-    }
+    };
     var successCb = function() {
-      $('.flash').remove();
+      $(".flash").remove();
     };
 
     var pollingCb = function(pollingInfo) {
       var mx = pollingInfo.maxNumTries;
       var cur = pollingInfo.numTries;
-      console.log("cur vs mx ", cur, mx);
+      // console.log("cur vs mx ", cur, mx);
       if (cur > mx / 30) {
-        if ( cur < mx / 15 ) {
-          flash('Boring...');
-        } else if ( cur < mx / 7 ) {
-          flash('Geez.  That server is soooo slow!');
-        } else if ( cur < mx / 4 ) {
-          flash('Maybe you should go have a cup of coffee while we work on this.');
-        } else if ( cur < mx / 2 ) {
-          flash("ARRG! These stupid computers.   Just give me my picture back!");
-        } else if ( cur >= (mx-1)) {
-          flash("Shit, man!  Things might be broke.  You could (in a separate browser) look try <a target='blank' href='"+pollingInfo.file+"'>this file</a> until it works.  Or just give up and file a bug.");
+        if (cur < mx / 15) {
+          flash("Boring...");
+        } else if (cur < mx / 7) {
+          flash("Geez.  That server is soooo slow!");
+        } else if (cur < mx / 4) {
+          flash(
+            "Maybe you should go have a cup of coffee while we work on this."
+          );
+        } else if (cur < mx / 2) {
+          flash(
+            "ARRG! These stupid computers.   Just give me my picture back!"
+          );
+        } else if (cur >= mx - 1) {
+          flash(
+            "Shit, man!  Things might be broke.  You could (in a separate browser) look try <a target='blank' href='" +
+              pollingInfo.file +
+              "'>this file</a> until it works.  Or just give up and file a bug."
+          );
         } else {
-          flash('Still Trying');
+          flash("Still Trying");
         }
       }
     };
 
-    $('.frame img').imagePoller({onPoll:pollingCb,onSuccess: successCb });
+    $(".frame img").imagePoller({ onPoll: pollingCb, onSuccess: successCb });
   }
-
-
 });
--- public/javascripts/spinit.js
+++ public/javascripts/spinit.js
@@ -1,32 +1,35 @@
-LTSpinner = (function() {
-
-  function LTSpinner(opts) {
-    this.opts = _.extend({
-      lines: 13,
-      length: 30,
-      width: 14,
-      radius: 40,
-      corners: 1,
-      rotate: 0,
-      direction: 1,
-      speed: 1,
-      trail: 60,
-      shadow: false,
-      hwaccel: false,
-      className: 'spinner',
-      zIndex: 2e9,
-      top: '160px',
-      left: '100%',
-      color: '#999',
-      element: 'spinner'
-    }, opts);
+GIUSpinner = (function() {
+  function GIUSpinner(opts) {
+    this.opts = _.extend(
+      {
+        lines: 13,
+        length: 30,
+        width: 14,
+        radius: 40,
+        corners: 1,
+        rotate: 0,
+        direction: 1,
+        speed: 1,
+        trail: 60,
+        shadow: false,
+        hwaccel: false,
+        className: "spinner",
+        zIndex: 2e9,
+        top: "160px",
+        left: "100%",
+        color: "#999",
+        element: "spinner"
+      },
+      opts
+    );
     this.spinner = new Spinner(this.opts);
   }
 
-  LTSpinner.prototype.spin = function() {
-    return this.spinner.spin(document.getElementById(this.opts.element || 'spinner'));
+  GIUSpinner.prototype.spin = function() {
+    return this.spinner.spin(
+      document.getElementById(this.opts.element || "spinner")
+    );
   };
 
-  return LTSpinner;
-
+  return GIUSpinner;
 })();
