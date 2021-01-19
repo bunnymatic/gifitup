741d832faf8f51a9e1486987e6428864ddcf6afc
Jon Rogers <jon@carbonfive.com>
2021-01-18 19:35:39 -0800

    Add marquee to the generate script fi there is only arg

--- scripts/generate.rb
+++ scripts/generate.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
 require_relative "../lib/image_processor"
+require_relative "../lib/word_processor"
 
 puts "Generating ..."
 
-puts ImageProcessor.new(ARGV).generate_animation({output_file: "generated.gif"})
+words = ARGV
+if ARGV.length == 1
+  words = WordProcessor.new(ARGV).marquee
+end
+puts ImageProcessor.new(words).generate_animation({output_file: "generated.gif", delay: 20})
