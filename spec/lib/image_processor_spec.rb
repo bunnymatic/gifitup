fdfaba2c7e2b758c577b27b7b5af7e1865abe795
Jon Rogers <jon@carbonfive.com>
2022-05-31 08:06:05 -0700

    Add and run rubocop

--- spec/lib/image_processor_spec.rb
+++ spec/lib/image_processor_spec.rb
@@ -1,41 +1,42 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe ImageProcessor do
-
-  describe ".generate_animation" do
+  describe '.generate_animation' do
     it 'calls convert the right number of times' do
       expect(MojoMagick).to receive(:convert).exactly(2).times
       described_class.new("it's like this").generate_animation
     end
     it 'calls convert with the right filename' do
       expect(MojoMagick).to receive(:convert).exactly(4).times
-      described_class.new(%w(it's like this)).generate_animation
+      described_class.new(%w[it's like this]).generate_animation
     end
     it 'includes fonts' do
-      font = MojoMagick::available_fonts.first.name
-      processor = described_class.new(%w(rock on))
+      font = MojoMagick.available_fonts.first.name
+      processor = described_class.new(%w[rock on])
       allow(processor).to receive(:generate_frame).and_call_original
-      processor.generate_animation({font: font})
-      expect(processor).to have_received(:generate_frame).with("rock", {font: font}).ordered
-      expect(processor).to have_received(:generate_frame).with("on", {font: font}).ordered
+      processor.generate_animation({ font: font })
+      expect(processor).to have_received(:generate_frame).with('rock', { font: font }).ordered
+      expect(processor).to have_received(:generate_frame).with('on', { font: font }).ordered
     end
   end
 
-  describe ".generate_filename" do
+  describe '.generate_filename' do
     context '.sanitize_filename' do
       it 'prefixes with destination' do
-        clean = described_class.new("the words").generate_filename('xxx')
+        clean = described_class.new('the words').generate_filename('xxx')
         expect(clean).to start_with('xxx/the_words')
         expect(clean).to end_with('.gif')
       end
       it 'does not include duplicate words' do
-        clean = described_class.new(%w( a a b b c c thing thing )).generate_filename('xxx')
+        clean = described_class.new(%w[a a b b c c thing thing]).generate_filename('xxx')
         expect(clean).to start_with 'xxx/abcthing'
         expect(clean).to end_with('.gif')
       end
       it 'doesn\'t return HUGE filenames' do
-        huge_prefix = 10.times.map{|x| 'this is the word'}.join
-        clean = described_class.new(huge_prefix).generate_filename( 'xxx')
+        huge_prefix = Array.new(10) { |_x| 'this is the word' }.join
+        clean = described_class.new(huge_prefix).generate_filename('xxx')
         # depending on `tmpfile` generation, this could be of different lengths
         # but it should prove that it is far less than the words used to genereate
         # the filename
--- spec/lib/word_processor_spec.rb
+++ spec/lib/word_processor_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe WordProcessor do
   describe '#marquee' do
-    it "builds a marquee list from the input words" do
+    it 'builds a marquee list from the input words' do
       expected = [
-        "a can ",
-        " can a",
-        "can a ",
-        "an a c",
-        "n a ca",
-        " a can",
+        'a can ',
+        ' can a',
+        'can a ',
+        'an a c',
+        'n a ca',
+        ' a can',
       ]
-      expect(WordProcessor.new("a can").marquee).to eq expected
+      expect(WordProcessor.new('a can').marquee).to eq expected
     end
   end
 end
