f1e4ae7a37fbe130f390f3b179205d9e6d7d8936
Jon Rogers <jon@carbonfive.com>
2023-06-24 11:01:09 -0700

    Bundle update

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ MAX_POLLS = 500
 POLL_INTERVAL = 0.4
 
 class Gifitup < Sinatra::Base
-
   set :static, true
   set :root, File.dirname(__FILE__)
   set :public_folder, 'public'
@@ -23,7 +22,8 @@ class Gifitup < Sinatra::Base
 
   get '/' do
     fetch_fonts
-    slim :index, :layout => :layout, :locals => {:frames => nil, :background => '#000000', :fonts => @fonts, :font => Font.default.name, :delay => 0.40 }
+    slim :index, layout: :layout,
+                 locals: { frames: nil, background: '#000000', fonts: @fonts, font: Font.default.name, delay: 0.40 }
   end
 
   post '/' do
@@ -45,47 +45,45 @@ class Gifitup < Sinatra::Base
 
     frames = []
 
-    if marquee
-      words = WordProcessor.new(words).marquee
-    end
+    words = WordProcessor.new(words).marquee if marquee
     if words && (words.length > 0)
       processor = ImageProcessor.new(words)
       outfile = processor.generate_filename(storage_directory)
 
       opts = {
-        :delay => delay.to_f * 100.0,
-        :font => font,
-        :pointsize => font_size,
-        :background => background,
-        :fill => fill,
-        :background_file => file,
-        :output_file => outfile,
-        :async => async
+        delay: delay.to_f * 100.0,
+        font: font,
+        pointsize: font_size,
+        background: background,
+        fill: fill,
+        background_file: file,
+        output_file: outfile,
+        async: async,
       }
 
-      Thread.new {
+      Thread.new do
         ImageProcessor.new(words).generate_animation(opts)
-      }
+      end
       frames = [asset_filename(outfile)]
     end
     locals = {
-      :frames => frames,
-      :fonts => @fonts
-    }.merge(params.slice(*%w(words font delay font_size background fill)).symbolize_keys)
-    slim :index, :locals => locals
+      frames: frames,
+      fonts: @fonts,
+    }.merge(params.slice(*%w[words font delay font_size background fill]).symbolize_keys)
+    slim :index, locals: locals
   end
 
   get '/gallery' do
-    limit = (params["limit"] || 10).to_i
+    limit = (params['limit'] || 10).to_i
     all_files = Dir.glob(File.join(storage_directory, '*.gif'))
-    sorted_files = all_files.sort_by{ |f| -1*File.mtime(f).to_i }
+    sorted_files = all_files.sort_by { |f| -1 * File.mtime(f).to_i }
     limit -= 1
-    trimmed_files = sorted_files[0..limit].map{|f| asset_filename(f)}
-    slim :gallery, :locals => {:files => trimmed_files}
+    trimmed_files = sorted_files[0..limit].map { |f| asset_filename(f) }
+    slim :gallery, locals: { files: trimmed_files }
   end
 
   def asset_filename(fname)
-    fname.gsub(/^\/?public\//,'/').gsub(/^\/*/,'/')
+    fname.gsub(%r{^/?public/}, '/').gsub(%r{^/*}, '/')
   end
 
   def fetch_fonts
@@ -103,10 +101,7 @@ class Gifitup < Sinatra::Base
   def save_file(destination_fname, uploaded_file)
     dest = File.join(settings.upload_directory, destination_fname)
     FileUtils.mkdir_p(settings.upload_directory)
-    File.open(dest, "w") do |f|
-      f.write(uploaded_file.read)
-    end
+    File.write(dest, uploaded_file.read)
     dest
   end
-
 end
--- lib/image_processor.rb
+++ lib/image_processor.rb
@@ -4,66 +4,64 @@ require 'mojo_magick'
 require 'logger'
 
 class ImageProcessor
-
   DEFAULT_OPTS = {
-    :background_file => nil,
-    :background => 'black',
-    :fill => 'white',
-    :gravity => 'center',
-    :font => 'Helvetica-Bold',
-    :pointsize => 64,
-    :size => '400x400',
-    :delay => 60
+    background_file: nil,
+    background: 'black',
+    fill: 'white',
+    gravity: 'center',
+    font: 'Helvetica-Bold',
+    pointsize: 64,
+    size: '400x400',
+    delay: 60,
   }
 
   attr_reader :words
-  def initialize(words, logger=nil)
+
+  def initialize(words, logger = nil)
     @logger = logger || Logger.new(STDOUT)
     @words = [words].flatten.compact
   end
 
   def generate_animation(opts = {})
-    @logger.info "generate_animation"
+    @logger.info 'generate_animation'
     outfile = opts.delete(:output_file)
     async = opts.delete(:async)
     unless outfile
       dest_dir = opts.delete(:dest_dir) || 'public/generated'
       outfile = generate_filename(dest_dir)
     end
-    dest_dir = File::dirname(outfile)
+    dest_dir = File.dirname(outfile)
     FileUtils.mkdir_p(dest_dir)
-
-    # this pins the memory usage on heroku - maybe we shouldn't allow this
 
     # resize the original image first
     tmpdir = Dir.mktmpdir
     if opts[:background_file]
       new_base = generate_frame_filename('bg', tmpdir)
-      MojoMagick::resize(opts[:background_file], new_base, :width => 400, :height => 400, :fill => true)
+      MojoMagick.resize(opts[:background_file], new_base, width: 400, height: 400, fill: true)
       opts[:background_file] = new_base
     end
     if async
-      @logger.info "generating async"
+      @logger.info 'generating async'
 
       threads = []
       frames = words.map do |word|
         tmpname = generate_frame_filename(word, tmpdir)
-        threads << Thread.new {
-          generate_frame(word, opts.merge({:tempname => tmpname} ))
-        }
+        threads << Thread.new do
+          generate_frame(word, opts.merge({ tempname: tmpname }))
+        end
         tmpname
       end
       threads.map(&:join)
     else
-      @logger.info "generating sync"
+      @logger.info 'generating sync'
       frames = words.map do |word|
         generate_frame(word, opts)
       end
     end
     opts = DEFAULT_OPTS.merge(opts)
 
-    MojoMagick::convert(nil,outfile) do |c|
-      c.delay (opts[:delay] || 0)
+    MojoMagick.convert(nil, outfile) do |c|
+      c.delay(opts[:delay] || 0)
       c.loop 0
       frames.each do |f|
         c.file f
@@ -82,53 +80,50 @@ class ImageProcessor
   end
 
   private
-  def generate_frame(word, opts)
-    begin
-      fname = opts.delete(:tempname)
-      unless fname
-        tmpdir = Dir.mktmpdir
-        fname = generate_frame_filename(word, tmpdir)
-      end
-      @logger.info "generating frame for #{word} in #{fname}"
 
-      opts = DEFAULT_OPTS.merge(opts)
-      opts[:pointsize] = 6 if opts[:pointsize].to_i < 6
-      src_file = opts.delete(:background_file)
-      resize = (opts[:size] + '^')
-      extent = opts[:size]
-      if src_file
-        opts[:background] = 'transparent'
-      end
-      opts.delete(:delay)
-      r = MojoMagick::convert(src_file, fname) do |c|
-        c.resize resize
-        c.gravity 'center'
-        c.extent extent
-        opts.each do |opt,val|
-          c.send(opt, val)
-        end
-        if src_file
-          c.annotate word, geometry: 0
-        else
-          c.label word
-        end
-      end
-      fname
-    rescue Exception => ex
-      @logger.warn "ACK #{ex}"
-      @logger.warn ex.backtrace
+  def generate_frame(word, opts)
+    fname = opts.delete(:tempname)
+    unless fname
+      tmpdir = Dir.mktmpdir
+      fname = generate_frame_filename(word, tmpdir)
     end
+    @logger.info "generating frame for '#{word}' in #{fname}"
+
+    opts = DEFAULT_OPTS.merge(opts)
+    opts[:pointsize] = 6 if opts[:pointsize].to_i < 6
+    src_file = opts.delete(:background_file)
+    resize = (opts[:size] + '^')
+    extent = opts[:size]
+    opts[:background] = 'transparent' if src_file
+    opts.delete(:delay)
+    r = MojoMagick.convert(src_file, fname) do |c|
+      c.resize resize
+      c.gravity 'center'
+      c.extent extent
+      opts.each do |opt, val|
+        c.send(opt, val)
+      end
+      if src_file
+        c.annotate word, geometry: 0
+      else
+        c.label word
+      end
+    end
+    fname
+  rescue Exception => e
+    @logger.warn "ACK #{e}"
+    @logger.warn e.backtrace
   end
 
   def temp_gif(pfx, dest_dir)
     fname = nil
-    Dir::Tmpname.create([pfx,'.gif']) do |path|
+    Dir::Tmpname.create([pfx, '.gif']) do |path|
       fname = File.join(dest_dir, File.basename(path))
     end
     fname
   end
 
   def sanitize_filename(fname)
-    fname.gsub(/[[:punct:]]/,'').gsub(/\s+/,'_')[0..49]
+    fname.gsub(/[[:punct:]]/, '').gsub(/\s+/, '_')[0..49]
   end
 end
